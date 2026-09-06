515a7ae69bdff57da7ad9a30f85ee5e2d4cde3e5
pavlobohush <bohushpavlo@gmail.com>
2026-09-06 22:34:29 +0200

    Change the greeting text

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hei!</h1><p>Servert fra mitt virtuelle miljø</p>"
+    return "<h1>Hello!</h1><p>Servert fra mitt virtuelle miljø</p>"
 
 
 if __name__ == "__main__":
